f948675a9076dee918cf4f9f555a67308ef04c34
Lowell Bander <lowellbander@gmail.com>
2014-09-20 20:39:12 -0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tourseasApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tourseasApp
+ */
+angular.module('tourseasApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tourseasApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tourseasApp
+ */
+angular.module('tourseasApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
